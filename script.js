17a9f9550d894e86fb8a0779b4fc8d5b153d828c
Speeckby <57104425+Speeckby@users.noreply.github.com>
2025-01-23 15:00:07 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleContent(int) {
-    const content = document.querySelectorAll('.content')[int];
-    const categorie = document.querySelectorAll('.categorie')[int];
-
-    content.classList.toggle('show');
-    categorie.classList.toggle('active');
-  }
-  
